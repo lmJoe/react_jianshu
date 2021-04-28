ba14815301eb1dce664d6211288c16819e2bf875
lim <lim@baomihua.com>
2021-04-28 11:26:31 +0800

    增加actionCreators和constants文件实现操作类型常量拆分、代码规范化

--- src/common/header/store/index.js
+++ src/common/header/store/index.js
@@ -2,5 +2,6 @@
 import reducer from './reducer';
 import * as actionCreators from './actionCreators';
 import * as constants from './constants';
+
 //将引入的reducer文件export出去作为header中store的出口文件，以此来减少大的store文件中reducer合并各个组件方法的引入路径
 export {reducer,actionCreators,constants};//将actionsCreators和constants暴露出去 以便于index.js作为该组件下store的唯一引入文件
