867edf9cad20e948870b53233eafb2ebbebfdee0
lim <lim@baomihua.com>
2021-04-28 10:29:08 +0800

    combineReducer用来合并由于项目的复杂性导致对大的reducer进行拆分

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';//combineReducers用于合并各个组件中的store中reducer文件中的方法
 import { reducer as headerReducer } from '../common/header/store'; //reducer as headerReducer意为把reducer比作headerReducer来使用
 
+
 export default combineReducers({
   header:headerReducer
 })
